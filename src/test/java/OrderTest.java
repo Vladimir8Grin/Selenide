ac49eb5a1558d2d4547801cd23f0c284f40e7bf6
“Vladimir_Grin” <mortywallet@gmail.com>
2023-12-07 22:13:14 +0300

    Bild1

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -21,12 +24,7 @@ public class OrderTest {
 
     @BeforeAll
     public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
+
         WebDriverManager.chromedriver().setup();
     }
 
@@ -55,13 +53,13 @@ public class OrderTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
         String datePlanning = generateDate(3, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.DELETE), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(datePlanning);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79999999999");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldBe(Condition.exactText("Встреча успешно забронирована на " + datePlanning));
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Встреча успешно забронирована на " + datePlanning));
 
     }
 }
