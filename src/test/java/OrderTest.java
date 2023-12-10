00c05c55dc828228730733e41abacd83958f38d7
“Vladimir_Grin” <mortywallet@gmail.com>
2023-12-10 21:37:04 +0300

    Bild2

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,56 +1,21 @@
 import com.codeborne.selenide.Condition;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-
 public class OrderTest {
-/*
-    private WebDriver driver;
 
-    @BeforeAll
-    public static void setupAll() {
-
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        driver.quit();
-        driver = null;
-    }
-*/
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
-
     @Test
-    public void TheForm() {
+    public void theForm() {
+        Configuration.headless = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
         String datePlanning = generateDate(3, "dd.MM.yyyy");
